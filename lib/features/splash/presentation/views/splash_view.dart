0ffa02f6d2546b5a6bd933a0bea4ebf637f2618f
omarabady <omarabady44@gmail.com>
2025-12-08 13:25:39 +0200

    Create SplashView UI

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fruitify/features/splash/presentation/views/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
   static const String routeName = 'splash';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(body: SplashViewBody());
   }
 }
