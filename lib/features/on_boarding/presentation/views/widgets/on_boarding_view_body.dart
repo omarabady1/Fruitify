c8cd65542ad4dd369ccea14b148be4467cb4ad0d
Omar Abady <omarabady44@gmail.com>
2025-12-22 01:09:57 +0200

    Create Dots Indicator

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:fruitify/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
@@ -6,6 +7,19 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Expanded(child: OnBoardingPageView())]);
+    return Column(
+      children: [
+        Expanded(child: OnBoardingPageView()),
+        DotsIndicator(
+          dotsCount: 2,
+          animate: true,
+          decorator: DotsDecorator(
+            activeColor: const Color(0xFF1B5E37),
+            color: const Color(0xFF5DB957).withValues(alpha: 0.5),
+            size: Size(9, 9),
+          ),
+        ),
+      ],
+    );
   }
 }
