6e4e694487646b94e1957e28de8b6550b4fb5e91
Omar Abady <omarabady44@gmail.com>
2026-01-07 15:06:17 +0200

    Setup Service Locator

---
+++ lib/core/functions/setup_service_locator.dart
@@ -0,0 +1,13 @@
+import 'package:fruitify/core/services/firebase_auth_service.dart';
+import 'package:fruitify/features/authentication/data/repos/auth_repo_implementation.dart';
+import 'package:get_it/get_it.dart';
+import '../../features/authentication/domain/repos/auth_repo.dart';
+
+final locator = GetIt.instance;
+
+void setupServiceLocator() {
+  locator.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  locator.registerSingleton<AuthRepo>(
+    AuthRepoImplementation(locator.get<FirebaseAuthService>()),
+  );
+}
